707532ccf79bb37f2cc4ca610c359418025aef10
Allam9573 <aallamdaniel@gmail.com>
2023-11-20 17:48:34 -0600

    modelos

--- src/main/java/com/unah/examen/modelos/Vehiculo.java
+++ src/main/java/com/unah/examen/modelos/Vehiculo.java
@@ -1,6 +1,9 @@
 package com.unah.examen.modelos;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,7 +20,8 @@ import lombok.Setter;
 public class Vehiculo {
 
     @Id
-    
+    @Column(name="idvehiculo")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idVehiculo;
     
 }
